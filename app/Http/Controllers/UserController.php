2d6e6226d23443c648a3511f72ad56d5b0fb7c69
Fernando Vega <juanfe.vega@gmail.com>
2020-02-01 19:21:47 -0500

    base estructure api users

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,116 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Http\Request;
+use App\User;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return User::all();
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'identification' => 'required|unique:users',
+            'email' => 'required|unique:users',
+        ]);
+
+        $errors = $validator->errors();
+        $fields = $request->all();
+
+        if ($validator->fails()) {
+            return response()->json($errors, 500);
+        } else {
+            return User::create($fields);
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return User::find($id);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $user = User::findOrFail($id);
+
+        $validator = Validator::make($request->all(), [
+            'identification' => 'required|unique:users',
+            'email' => 'required|unique:users',
+        ]);
+
+        $errors = $validator->errors();
+
+        $user->update($request->all());
+
+        if ($validator->fails()) {
+            return response()->json($errors, 201);
+        } else {
+            $user->update($request->all());
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $user = User::FindOrFail($id);
+        $user->delete();
+
+        return 204;
+    }
+}
